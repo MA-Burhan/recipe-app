becf7c2f97e85b085b9f223a309df66db0b3e2e9
Amin <amin.burhan@gmail.com>
2021-09-27 19:53:14 +0200

    Add Spring Data repository for UnitOfMeasurement.

---
+++ src/main/java/maburhan/recipeapp/repositories/UnitOfMeasurementRepository.java
@@ -0,0 +1,7 @@
+package maburhan.recipeapp.repositories;
+
+import maburhan.recipeapp.model.UnitOfMeasurement;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasurementRepository extends CrudRepository<UnitOfMeasurement, Long> {
+}
