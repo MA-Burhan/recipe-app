a2364d8ddaea5ce60cd91fa340967074b6a90409
Mohamed-Amin Burhan <amin.burhan@gmail.com>
2021-12-24 23:33:10 +0100

    Add unit test for Recipe service layer.

---
+++ src/test/java/maburhan/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,41 @@
+package maburhan.recipeapp.services;
+
+import maburhan.recipeapp.model.Recipe;
+import maburhan.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+    }
+}
